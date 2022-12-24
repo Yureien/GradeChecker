c65636163649cf8128d5d1503091a33b134cebf2
Soham Sen <hello@sohamsen.me>
2022-12-24 06:49:08 +0530

    Pls work

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-python manage.py collectstatic --no-input
-python manage.py compilemessages
+python manage.py ./collectstatic --no-input
+python manage.py ./compilemessages
 
-python manage.py migrate
+python manage.py ./migrate
 
 exec "${@}"
